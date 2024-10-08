2207f6161bcf2bcacb971faee81ced008383e831
mohyeah <2565495564@qq.com>
2024-10-08 19:58:07 +0800

    Initial commit

---
+++ book 2.3.1.py
@@ -0,0 +1,28 @@
+import time
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+
+def test2():
+    l = []
+    for i in range(1000):
+        l.append(i)
+
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
+    l = list(range(1000))
+
+def test_time(test):
+    t1 = time.time()
+    test()
+    t2 = time.time()
+    print(t2-t1)
+
+test_time(test1)
+test_time(test2)
+test_time(test3)
+test_time(test4)
+
