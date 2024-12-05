f1b22c2ea93df0df973314137e6571a479a6595f
mohyeah <2565495564@qq.com>
2024-12-05 15:01:29 +0800

    24/12/05 TCP服务端开发扩展

--- AI Learn/Stage 2 Python Advanced/Chapter 1 Python Advanced Programing/7 1-7 TCP服务器开发/TCP 服务器端开发(对话).py
+++ AI Learn/Stage 2 Python Advanced/Chapter 1 Python Advanced Programing/7 1-7 TCP服务器开发/TCP 服务器端开发(对话).py
@@ -1,8 +1,16 @@
 import socket
 
+# 1. 创建服务器端套接字对象
 tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+# ※ 设置端口复用, 程序执行结束, 让其占用的端口可以立即释放
+tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+
+# 2. 绑定服务器地址和端口号
 tcp_server_socket.bind(("", 9900))
+# 3. 监听客户端连接
 tcp_server_socket.listen(128)
 while True:
     # 使用try..except捕获链接异常
@@ -11,15 +19,20 @@ while True:
         # 循环接收客户端请求 (单线程版)
         while True:
             try:
-                recv_data = new_socket.recv(1024).decode("utf-8")
-                if len(recv_data) >= 1: # 忽略空数据
-                    print(f"{ip_port}发送的数据为:{recv_data}")
+                print(f"{ip_port}客户端已连接")
 
+                recv_data = new_socket.recv(1024).decode("utf-8")   # 接收客户端的字节流
+
+                if len(recv_data) != 0: # 忽略空数据, 若客户端已下线, 则接收的字节流长度为0
+                    print(f"{ip_port}发送的数据为:{recv_data}")
                     content = input("服务器返回数据:").encode("utf-8")
                     new_socket.send(content)
-
+                else:
+                    # 客户端已断开连接
+                    raise ConnectionResetError
             except ConnectionResetError:
                 print(f"{ip_port}客户端已断开")
+                new_socket.close()
                 break
     except:
         print("服务器异常")
