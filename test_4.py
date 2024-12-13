e32b740bd60a20848563a559946a1915469d3745
mohyeah <146638644+mohyeah@users.noreply.github.com>
2024-12-13 21:50:58 +0800

    Delete test_4.py

--- test_4.py
+++
@@ -1,9 +0,0 @@
-status_code = int(input("please input the code:"))
-match status_code:
-    case 400: descrp = 'bad'
-    case 401: descrp = 'unauthorized'
-    case 402: descrp = 'forbidden'
-    case 403: descrp = 'not found'
-    case 404: descrp = 'not allowed'
-    case _: descrp = 'unknown code'
-print('description:', descrp)
