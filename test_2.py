14a31081c57b833c6a4dc5cf731d7d07761e3b81
mohyeah <146638644+mohyeah@users.noreply.github.com>
2024-12-13 21:46:18 +0800

    Delete test_2.py

--- test_2.py
+++
@@ -1,6 +0,0 @@
-radius = float(input("please input the radius:"))
-perimeter = 2 * 3.14 * radius
-area = 3.14 * radius**2
-print('C:%.2f'% perimeter)
-print(f'C:{perimeter:.1f}')
-print('S:%.2f'% area)
