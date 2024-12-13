cdab590058a2dbf79a0e9f97b4011b085525f74d
mohyeah <146638644+mohyeah@users.noreply.github.com>
2024-12-13 21:51:48 +0800

    Delete test_9.py

--- test_9.py
+++
@@ -1,7 +0,0 @@
-import random as r
-counter = [0] * 6
-for _ in range(6000):
-    face = r.randrange(1, 7)
-    counter[face - 1] += 1
-for face in range(1, 7):
-    print(f'{face} appear {counter[face - 1]} times')
