e4d63c2e330999a2e432b0b990ecf4bbad269a3b
mohyeah <146638644+mohyeah@users.noreply.github.com>
2024-12-13 21:51:39 +0800

    Delete test_8.py

--- test_8.py
+++
@@ -1,5 +0,0 @@
-for x in range(0, 21):
-    for y in range(0, 34):
-        for z in range(0, 100, 3):
-            if x + y + z == 100 and 5 * x + y + z // 3 == 100:
-                print(f'Mc:{x}, Fc:{y}, Sc:{z}')
